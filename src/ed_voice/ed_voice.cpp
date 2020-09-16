7fb99c7bc94cb501a18f0c5835188810b01430c7
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2020-09-16 21:55:16 +0900

    Added ed_voice.cpp & ed_voice.h

---
+++ src/ed_voice/ed_voice.cpp
@@ -0,0 +1,17 @@
+#include "ed_voice.h"
+
+int __cdecl Init() {
+    return 0;
+}
+
+int __cdecl Uninit() {
+    return 0;
+}
+
+int __cdecl Start() {
+    return 0;
+}
+
+int __cdecl End() {
+    return 0;
+}
---
+++ src/ed_voice/ed_voice.h
@@ -0,0 +1,9 @@
+#ifndef ED_VOICE_H_
+#define ED_VOICE_H_
+
+extern "C" int __cdecl Init();
+extern "C" int __cdecl Uninit();
+extern "C" int __cdecl Start();
+extern "C" int __cdecl End();
+
+#endif // !ED_VOICE_H_
