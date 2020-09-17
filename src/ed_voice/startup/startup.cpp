220dde7d1db60bab9a764dbf95110c60befd8ae6
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2020-09-18 00:16:56 +0900

    Add startup

---
+++ src/ed_voice/startup/startup.cpp
@@ -0,0 +1,28 @@
+#include "startup.h"
+
+namespace startup {
+class StartUpImpl : public StartUp {
+
+public:
+	bool Search() override;
+	bool Inject() override;
+	bool Start() override;
+};  // StartUpImpl
+
+std::unique_ptr<StartUp> StartUp::GetInstance() {
+	return std::make_unique<StartUpImpl>();
+}
+
+bool StartUpImpl::Search() {
+	return false;
+}
+
+bool StartUpImpl::Inject() {
+	return false;
+}
+
+bool StartUpImpl::Start() {
+	return false;
+}
+
+}  // namespace startup
---
+++ src/ed_voice/startup/startup.h
@@ -0,0 +1,19 @@
+#ifndef __STARTUP_STARTUP_H__
+#define __STARTUP_STARTUP_H__
+
+#include <memory>
+
+namespace startup {
+class StartUp {
+public:
+	virtual bool Search() = 0;
+	virtual bool Inject() = 0;
+	virtual bool Start() = 0;
+
+	virtual ~StartUp() {};
+
+	static std::unique_ptr<StartUp> GetInstance();
+};  // StartUp
+}  // namespace startup
+
+#endif // __STARTUP_STARTUP_H__
