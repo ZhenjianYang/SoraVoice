4e9d985ea7e505d2776d138f4bd07b7f89c1e125
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2020-09-18 02:02:24 +0900

    Add test for utils:str

---
+++ src/test/utils/str_test.cpp
@@ -0,0 +1,34 @@
+#include <string>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "utils/str.h"
+
+namespace {
+using std::string;
+using ::testing::ElementsAre;
+using ::utils::StrSplit;
+}  // namespace
+
+TEST(StrTest, StrSplitSkipEmpty) {
+    EXPECT_THAT(StrSplit("12#34#56", '#'), ElementsAre("12", "34", "56"));
+    EXPECT_THAT(StrSplit("123456", '#'), ElementsAre("123456"));
+
+    EXPECT_THAT(StrSplit("#123456", '#'), ElementsAre("123456"));
+    EXPECT_THAT(StrSplit("123456#", '#'), ElementsAre("123456"));
+    EXPECT_THAT(StrSplit("123##456", '#'), ElementsAre("123", "456"));
+    EXPECT_THAT(StrSplit("###", '#'), ElementsAre());
+    EXPECT_THAT(StrSplit("", '#'), ElementsAre());
+}
+
+TEST(StrTest, StrSplitKeepEmpty) {
+    EXPECT_THAT(StrSplit("12#34#56", '#', false), ElementsAre("12", "34", "56"));
+    EXPECT_THAT(StrSplit("123456", '#', false), ElementsAre("123456"));
+
+    EXPECT_THAT(StrSplit("#123456", '#', false), ElementsAre("", "123456"));
+    EXPECT_THAT(StrSplit("123456#", '#', false), ElementsAre("123456", ""));
+    EXPECT_THAT(StrSplit("123##456", '#', false), ElementsAre("123", "", "456"));
+    EXPECT_THAT(StrSplit("###", '#', false), ElementsAre("", "", "", ""));
+    EXPECT_THAT(StrSplit("", '#', false), ElementsAre(""));
+}
