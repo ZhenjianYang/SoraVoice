7f8087fce137c586da67fae1ba770c882e502f5f
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2020-09-18 00:18:43 +0900

    Update ed_voice

--- src/ed_voice/ed_voice.cpp
+++ src/ed_voice/ed_voice.cpp
@@ -1,5 +1,42 @@
 #include "ed_voice.h"
 
+#include <startup/startup.h>
+#include <utils/log.h>
+
+namespace {
+enum StartUpErrorCode {
+    kSucceeded = 0,
+    kSearchFailed,
+    kInjectFailed,
+    kStartFailed
+};
+}  // namesapce
+
 int __cdecl StartUp() {
-    return 0;
+    static bool succeeded = false;
+
+    LOG("Start Up...");
+    if (succeeded) {
+        LOG("Already Started.");
+        return kSucceeded;
+    }
+
+    auto startup = startup::StartUp::GetInstance();
+    if (!(succeeded = startup->Search())) {
+        LOG("Start Up Failed: Search Failed");
+        return kSearchFailed;
+    }
+
+    if (!(succeeded = startup->Inject())) {
+        LOG("Start Up Failed: Inject Failed");
+        return kInjectFailed;
+    }
+
+    if (!(succeeded = startup->Start())) {
+        LOG("Start Up Failed: Start Failed");
+        return kStartFailed;
+    }
+
+    LOG("Start Up Succeeded.");
+    return kSucceeded;
 }
