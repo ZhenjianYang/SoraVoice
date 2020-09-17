817412554bd26e5142ab8eec1173e41047fc36f9
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2020-09-18 02:01:53 +0900

    Update str.h

--- src/ed_voice/utils/str.h
+++ src/ed_voice/utils/str.h
@@ -5,14 +5,22 @@
 #include <vector>
 
 namespace utils {
-inline static std::vector<std::string> StrSplit(std::string_view s, char delimiter) {
+inline static std::vector<std::string> StrSplit(std::string_view s, char delimiter, bool skip_empty = true) {
+    if (s.empty() && !skip_empty) {
+        return { "" };
+    }
+
     std::vector<std::string> splits;
     size_t pos = 0;
     while (pos < s.length()) {
         size_t pos_next = s.find(delimiter, pos);
-        splits.emplace_back(s.substr(pos, pos_next - pos));
+        if (!skip_empty || pos_next > pos) {
+            splits.emplace_back(s.substr(pos, pos_next - pos));
+        }
         if (pos_next == std::string_view::npos) {
             break;
+        } else if (pos_next + 1 == s.length() && !skip_empty) {
+            splits.emplace_back("");
         }
         pos = pos_next + 1;
     }
