8d06bd99415242d6842185416ca1ac06a89ed03f
Zhenjian Yang <zhenjian.c.yang@gmail.com>
2020-09-21 20:53:06 +0900

    Format Clean: scan_group

--- src/ed_voice/startup/scan_group.h
+++ src/ed_voice/startup/scan_group.h
@@ -6,20 +6,20 @@
 #include <vector>
 
 namespace startup {
-    class ScanGroup {
-    public:
-        virtual bool IsValid() const = 0;
-        virtual const std::string& Name() const = 0;
-        virtual const std::string& GameTitle() const = 0;
-        virtual const std::string& GameBuiltDate() const = 0;
-        virtual std::vector<std::unique_ptr<char[]>>&& MovableStrings() = 0;
-        virtual bool Scan() = 0;
-        virtual bool Inject() = 0;
+class ScanGroup {
+public:
+    virtual bool IsValid() const = 0;
+    virtual const std::string& Name() const = 0;
+    virtual const std::string& GameTitle() const = 0;
+    virtual const std::string& GameBuiltDate() const = 0;
+    virtual std::vector<std::unique_ptr<char[]>>&& MovableStrings() = 0;
+    virtual bool Scan() = 0;
+    virtual bool Inject() = 0;
 
-        virtual ~ScanGroup() { }
+    virtual ~ScanGroup() { }
 
-        static std::vector<std::unique_ptr<ScanGroup>> GetScanGroups();
-    };  // ScanGroup
+    static std::vector<std::unique_ptr<ScanGroup>> GetScanGroups();
+};  // ScanGroup
 }  // namespace startup
 
 #endif // __STARTUP_SCAN_GROUP_H__
